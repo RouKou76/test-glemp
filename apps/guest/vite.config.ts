8916e2a17b8ddfa04feeadb063341353188144d1
gleb <smirnovg2005@gmail.com>
2026-06-27 20:21:23 +0300

    test for legacy2

--- apps/guest/vite.config.ts
+++ apps/guest/vite.config.ts
@@ -1,25 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'Android >= 7'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
-  ],
-
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-
   build: {
-    target: ['es2015'],
-    polyfillDynamicImport: true,
+    target: 'es2015',
+  },
+  server: {
+    port: 5173,
   },
 })
